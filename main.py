8bd3bd478dd535ca70e06baffa1a8ecca533f457
Issac Liu <issac@matchatea.dev>
2025-03-01 16:43:27 -0600

    refactor: improve key press and release logging format

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from pynput.keyboard import Key, Listener
 
+
 def on_press(key):
-    print('{0} pressed'.format(
-        key))
+    print(f"[KEY↓] {key} pressed")
+
 
 def on_release(key):
-    print('{0} release'.format(
-        key))
+    print(f"[KEY↑] {key} release")
     if key == Key.esc:
         # Stop listener
         return False
 
+
 # Collect events until released
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
