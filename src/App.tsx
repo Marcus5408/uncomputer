164fdfe99af6766cf84423fce196b39a4d75ba50
meepstertron <meepstertron@gmail.com>
2025-03-01 23:31:21 -0600

    add sy-background class to App component for styling consistency

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-gray-800 text-white h-screen flex flex-col items-center justify-center">
+    <div className="bg-gray-800 text-white h-screen flex flex-col items-center justify-center sy-background">
       <p>hello world :3</p>
     </div>
   );
