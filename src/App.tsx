b9a76a7dc10000014ff9b66f642fa3dfa2bd0f3b
spectralo <arthur.hardy@mageekbox.net>
2025-03-01 22:43:15 -0600

    added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-import { appWindow } from "@tauri-apps/plugin-window";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
-      <p>hello world :3</p>
-    </>
+    <div className="h-screen bg-black">
+      <h1 className="pt-8 font-bold text-xl">UnComputer v0.1</h1>
+      <Button>Click me</Button>
+    </div>
   );
 }
 
